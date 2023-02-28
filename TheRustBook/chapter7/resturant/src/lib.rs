031af2d9b0dc10b7c2c35e6845fee9daba1d0089
jeeva <jeeva.ramu@codurance.com>
2023-02-28 10:47:25 +0100

    end of chapter 7
    objectives
    monstly around packages and modules

---
+++ TheRustBook/chapter7/resturant/src/lib.rs
@@ -0,0 +1,26 @@
+mod front_of_house {
+
+    mod hosting {
+        fn add_to_waitlist() {
+
+        }
+
+        fn seat_at_table() {
+
+        }
+    }
+
+    mod servicing {
+        fn take_order() {
+
+        }
+
+        fn service_order() {
+
+        }
+
+        fn take_payment() {
+            
+        }
+    }
+}
