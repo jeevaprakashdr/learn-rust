5e2b9bdd4a45e8730246eccf146a40f473304497
jeeva <jeeva.ramu@codurance.com>
2023-03-13 15:47:50 +0000

    Chapter 13: part 1
    main objective
    closure

--- TheRustBook/chapter13/closures/src/main.rs
+++ TheRustBook/chapter13/closures/src/main.rs
@@ -1,10 +1,4 @@
-use std::{thread, time::Duration};
-
-fn simulated_expensive_calculation(intensity: i32) -> i32 {
-    println!("calculating slowly !!");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
+use std::{thread, time::Duration, collections::{HashMap}};
 
 fn main() {
     let simulated_intensity = 10;
@@ -13,15 +7,59 @@ fn main() {
     generate_workout(simulated_intensity, simulated_random_number);
 }
 
-fn generate_workout(intensity: i32, random_number: i32) {
+struct Cacher<T>
+where T: Fn(u32) -> u32, // Fn is a trait bound 
+{
+    calculation: T,
+    value: Option<u32>,
+    hasmap_value : HashMap<u32, u32>
+}
+
+impl<T> Cacher<T>
+where T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher { calculation, value: None, hasmap_value: HashMap::new() }
+    }
+
+    fn value(&mut self, arg: u32) -> u32{
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+
+    fn hash_map_value(&mut self, arg: u32) -> u32 {
+        match self.hasmap_value.get_key_value(&arg) {
+            Some(t) =>  *t.0,
+            None => {
+                let v = (self.calculation)(arg);
+                self.hasmap_value.insert(arg, v);
+                v
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut cache_result = Cacher::new(|num| {
+        println!("calculating slowly !!");
+        thread::sleep(Duration::from_secs(2)); 
+        num
+    });
+
     if intensity < 25 {
-        println!("Today, do {} pushups", simulated_expensive_calculation(intensity));
-        println!("Next, do {} situps", simulated_expensive_calculation(intensity));
+        println!("Today, do {} pushups", cache_result.hash_map_value(intensity));
+        println!("Next, do {} situps", cache_result.hash_map_value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today and rest well");
         }else{
-            println!("Today, run for {} minuets", simulated_expensive_calculation(intensity));
+            println!("Today, run for {} minuets", cache_result.value(intensity));
         }
     }
 }
